7fbec041143a4db98868300470a766f0681daed2
Закиров Алихан <alihanz1220@gmail.com>
2020-01-18 19:37:05 +0300

    cleanup

--- hw-2/main.go
+++ hw-2/main.go
@@ -1,17 +1,4 @@
 package main
 
-import (
-	"fmt"
-	"github.com/alikhanz/golang-otus-tasks/hw-2/unpacker"
-	"log"
-)
-
 func main() {
-	u := unpacker.New()
-
-	res, err := u.Unpack(`abcd\5`)
-
-	log.Println(err)
-
-	fmt.Print(res)
 }
