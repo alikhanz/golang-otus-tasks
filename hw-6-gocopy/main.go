48db6cf502b65c09fb8932118561180b6a30c902
Закиров Алихан <alihanz1220@gmail.com>
2020-01-30 02:59:15 +0300

    hw-6 finish

--- hw-6-gocopy/main.go
+++ hw-6-gocopy/main.go
@@ -1,21 +1,140 @@
 package main
 
 import (
+	"errors"
 	"flag"
+	"github.com/cheggaaa/pb/v3"
+	"github.com/gookit/color"
+	"io"
+	"os"
 )
 
 func main() {
 	var from, to string
-	var limit, offset int
+	var limit, offset int64
 
 	flag.StringVar(&from, "from", "", "/path/to/file")
 	flag.StringVar(&to, "to", "", "/path/to/file")
-	flag.IntVar(&limit, "limit", 0, "1024")
-	flag.IntVar(&offset, "offset", 0, "1024")
+	flag.Int64Var(&limit, "limit", 0, "1024")
+	flag.Int64Var(&offset, "offset", 0, "1024")
 
 	flag.Parse()
+
+	c := NewGoCopy()
+
+	err := c.Copy(from, to, limit, offset)
+
+	if err != nil {
+		color.SetOutput(os.Stderr)
+		color.Error.Println(err.Error())
+	}
 }
 
-func Copy(src, dst string, limit, offset int) error {
+const ReadChunkSize = 1024*4 // 4KB
+
+type GoCopy struct {
+	barEnabled bool
+}
+
+func NewGoCopy() GoCopy {
+	return GoCopy{barEnabled:true}
+}
+
+func (g *GoCopy) Copy(src, dst string, limit, offset int64) (err error) {
+	srcFile, err := os.OpenFile(src, os.O_RDONLY, 0755)
+	defer func() {
+		cerr := srcFile.Close()
+
+		if err == nil {
+			err = cerr
+		}
+	}()
+
+	if err != nil {
+		return err
+	}
+
+	stat, err := srcFile.Stat()
+
+	if err != nil {
+		return err
+	}
+
+	if stat.IsDir() {
+		return errors.New(`cannot copy directory`)
+	}
+
+	dstFile, err := os.OpenFile(dst, os.O_WRONLY|os.O_CREATE, 0755)
+	defer func() {
+		cerr := dstFile.Close()
+
+		if err == nil {
+			err = cerr
+		}
+	}()
+
+
+	if err != nil {
+		return err
+	}
+
+	if offset > stat.Size() {
+		return errors.New("offset cannot be larger than file size")
+	}
+
+	if limit < 0 {
+		return errors.New("limit cannot be negative")
+	}
+
+	if limit == 0 {
+		limit = stat.Size() - offset
+	}
+
+	if limit <= 0 {
+		return errors.New("incorrect size")
+	}
+
+	_, err = srcFile.Seek(offset, io.SeekStart)
+
+	reader := io.LimitReader(srcFile, limit)
+	bar := pb.New64(limit)
+
+	if g.barEnabled {
+		bar.Start()
+	}
+
+	buf := make([]byte, ReadChunkSize)
+	writer := bar.NewProxyWriter(dstFile)
+
+	for {
+		n, err := reader.Read(buf)
+		buf := buf[:n]
+
+		if n == 0 {
+			if err == nil {
+				continue
+			}
+			if err == io.EOF {
+				err = nil
+				break
+			}
+			return err
+		}
+
+		if err != nil && err != io.EOF {
+			bar.Finish()
+			return err
+		}
+
+		_, err = writer.Write(buf)
+
+		if err != nil {
+			bar.Finish()
+			return err
+		}
+	}
+
+	bar.Finish()
+
 	return nil
 }
---
+++ hw-6-gocopy/main_test.go
@@ -0,0 +1,102 @@
+package main
+
+import (
+	"fmt"
+	"github.com/stretchr/testify/assert"
+	"gotest.tools/golden"
+	"os"
+	"path/filepath"
+
+	//"path/filepath"
+	"testing"
+)
+
+func TestMain(m *testing.M) {
+	m.Run()
+
+	files, _ := filepath.Glob("testdata/*")
+	for _, f := range files {
+		if f != ".gitignore" {
+			exclude, _ := filepath.Match("*/.gitignore", f)
+			if !exclude {
+				_ = os.Remove(f)
+			}
+		}
+	}
+}
+
+func TestSuccessFullCopy(t *testing.T) {
+	makeTmpFiles(t, "1234567890", "1234567890")
+	err := copyTestFile(t, 0, 0)
+	compareFiles(t)
+	assert.NoError(t, err)
+}
+
+func TestSuccessLimitCopy(t *testing.T) {
+	makeTmpFiles(t, "1234567890", "12345")
+	err := copyTestFile(t, 5, 0)
+	compareFiles(t)
+	assert.NoError(t, err)
+}
+
+func TestSuccessOffsetCopy(t *testing.T) {
+	makeTmpFiles(t, "1234567890", "67890")
+	err := copyTestFile(t, 0, 5)
+	compareFiles(t)
+	assert.NoError(t, err)
+}
+
+func TestSuccessLimitAndOffsetCopy(t *testing.T) {
+	makeTmpFiles(t, "1234567890", "67890")
+	err := copyTestFile(t, 5, 5)
+	compareFiles(t)
+	assert.NoError(t, err)
+}
+
+func TestIncorrectOffsetCopy(t *testing.T) {
+	err := copyTestFile(t, 0, 10)
+	assert.Error(t, err)
+}
+
+func TestIncorrectLimitCopy(t *testing.T) {
+	err := copyTestFile(t, -10, 0)
+	assert.Error(t, err)
+}
+
+func TestIncorrectPath(t *testing.T) {
+	err := copyTestFile(t, 0,0)
+	assert.Error(t, err)
+}
+
+func copyTestFile(t *testing.T, limit, offset int64) error {
+	c := NewGoCopy()
+	c.barEnabled = false
+
+	return c.Copy(
+		golden.Path(t.Name()+".input"),
+		golden.Path(t.Name()+".output"),
+		limit,
+		offset,
+	)
+}
+
+func makeFile(t *testing.T, value, postfix string) {
+	inputFile, err := os.OpenFile(golden.Path(t.Name()+postfix), os.O_WRONLY|os.O_CREATE, 0755)
+
+	if err != nil {
+		panic(fmt.Sprintf("Cannot create test file. \n %#v", err))
+	}
+
+	defer inputFile.Close()
+
+	_, _ = inputFile.WriteString(value)
+}
+
+func makeTmpFiles(t *testing.T, inputValue, expectedValue string) {
+	makeFile(t, inputValue, ".input")
+	makeFile(t, expectedValue, ".golden")
+}
+
+func compareFiles(t *testing.T) {
+	golden.AssertBytes(t, golden.Get(t, t.Name()+".output"), t.Name()+".golden")
+}
