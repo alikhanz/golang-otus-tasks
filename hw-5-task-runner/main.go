fe65ee3a9a1c2c84cfb0a04acb749d7604f3f349
Закиров Алихан <alihanz1220@gmail.com>
2020-02-02 02:16:03 +0300

    cleanup

--- hw-5-task-runner/main.go
+++ hw-5-task-runner/main.go
@@ -1,35 +1,5 @@
 package main
 
-import (
-	"fmt"
-	"time"
-)
-
 func main() {
-	testChan := make(chan struct{})
 
-	go func(ch chan struct{}) {
-		for {
-			select {
-				case _, ok := <- ch:
-					fmt.Println("!", ok)
-					return
-			}
-		}
-	}(testChan)
-
-	go func(ch chan struct{}) {
-		for {
-			select {
-				case _, ok := <- ch:
-					fmt.Println("!!", ok)
-					return
-			}
-		}
-	}(testChan)
-
-	time.Sleep(1*time.Second)
-	testChan <- struct{}{}
-
-	time.Sleep(2*time.Second)
 }
